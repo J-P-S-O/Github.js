2b3268f11613cb134881d08ca958a30990c88a95
J-p-s-o <jpso.guerreiro@gmail.com>
2021-01-22 10:18:51 -0300

    .

--- server/index.js
+++ server/index.js
@@ -4,25 +4,30 @@ import bodyParser from "body-parser"
 import express from "express"
 export class GithubjsServer {
   constructor(path){
+    if (!path.startsWith("/")) path = "/" + path
     this.connector = new EventEmitter()
     this.payloadPath = path
     this.app = express()
     this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.raw());
+    this.debug = {}
+
     this.app.post(this.payloadPath,function(req, res){
       app.connector.emit("Got Body", req.body)
+      res.write(200)
     }
     )
   }
-  listen (port, cb){
-    let defaultcb = () => {
-      console.error("Succesfully loaded GithubjsServer")
-      console.error("Check if the port forwarder is ok")
-      console.error("And that webhooks are on")
-      console.error("That's all! :)")
-    }
-    this.app.listen(port, cb || defaultcb)
+  listen (port){
+
+     var p = this.app.listen(port, function() {
+       console.error("Succesfully loaded GithubjsServer")
+       console.error("Check if the port forwarder is ok")
+       console.error("And that webhooks are on")
+       console.error("That's all! :)")
+       this.debug.port = p.address().port
+     })
   }
 
 }
--- test/index.js
+++ test/index.js
@@ -1,1 +1,4 @@
-import  GithubjsServer from "./../server/index"
+import  { GithubjsServer } from "./../server/index"
+let server = new GithubjsServer("payload")
+server.listen(0)
+console.error(server.app.port)
